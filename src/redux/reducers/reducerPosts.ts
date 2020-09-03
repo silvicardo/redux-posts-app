e7605cbbfa5e7c09fabd8392cb48c4c3713167d5
riccardosilvi@armadioverde.it <riccardosilvi@armadioverde.it>
2020-09-03 13:01:08 +0200

    sagaPostList

--- src/redux/reducers/reducerPosts.ts
+++ src/redux/reducers/reducerPosts.ts
@@ -1,9 +1,13 @@
 import { Utility } from "../../utils/Utility";
+import { FETCH_POSTS_LIST_SUCCESS } from "../actions";
 
 export const reducerPosts = (prevState: any = {}, action: any) => {
   let clonedState = Utility.clone(prevState);
 
   switch (action.type) {
+    case FETCH_POSTS_LIST_SUCCESS:
+      clonedState.list = action.payload;
+      break;
     default:
       break;
   }
--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -1,6 +1,26 @@
-import { put } from "redux-saga/effects";
-import { FETCH_POSTS_LIST_SUCCESS } from "../actions";
+import { put, call } from "redux-saga/effects";
+import { FETCH_POSTS_LIST_SUCCESS, FETCH_POSTS_LIST_FAILURE } from "../actions";
+
+// const apiList = () => {
+//   return fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((res) => res.json()) //JSON.parse
+//     .then((result) => result);
+// };
+
+const apiList = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return response.json();
+};
 
 export function* sagaPostList() {
-  yield put({ type: FETCH_POSTS_LIST_SUCCESS, payload: { posts: [] } });
+  try {
+    //API
+    const response = yield call(apiList);
+    yield put({ type: FETCH_POSTS_LIST_SUCCESS, payload: response });
+  } catch (e) {
+    yield put({
+      type: FETCH_POSTS_LIST_FAILURE,
+      payload: { message: e.message },
+    });
+  }
 }
